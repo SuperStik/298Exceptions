9b22c888cd6634ab88c5c8147df1a08f55ee7ea1
SuperStik <daviswood290@gmail.com>
2024-05-19 16:59:35 -0700

    Add sigma function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+// Sum the integers from 1 to n. For example, if n is 5, then
+// sigma returns the sum 1 + 2 + 3 + 4 + 5 == 15.
+// @param n integer upper bound of summation
+// @pre n >= 1
+// @post return value is the sum of the integers from 1 to n.
+// @return sum of the integers from 1 to n, where n is
+//         specified as an argument or 0 if an error occurred.
+unsigned long sigma(unsigned long n) {
+	unsigned long sum = 0;
+
+	return sum;
+}
+
 int main() {
 	return 0;
 }
